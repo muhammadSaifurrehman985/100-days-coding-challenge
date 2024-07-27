72e5e1face6be27547a10d1889cbbb9cf91da734
muhammadSaifurrehman985 <159415417+muhammadSaifurrehman985@users.noreply.github.com>
2024-07-27 11:40:50 +0500

    Add files via upload
    Here are the topics covered in the repository:
    
    Installing Node.js, TypeScript, and VS Code
    Personal Message
    Name Cases
    Summary of Each Topic
    Installing Node.js, TypeScript, and VS Code:
    
    Node.js: JavaScript runtime for server-side code. Install from the Node.js website.
    TypeScript: Typed superset of JavaScript. Install via npm.
    VS Code: Code editor by Microsoft. Install from the VS Code website.
    Personal Message:
    
    Store a name in a variable and print a greeting message.
    Name Cases:
    
    Print a name in lowercase, uppercase, and title case

---
+++ DAY 1/app1.js
@@ -0,0 +1,13 @@
+"use strict";
+// QUESTION :2 OF 100 DAYS CHALLENGE
+let personName = "Asharib";
+let personalMessage = `"Hello ${personName}, would you like to learn some TypeScript today?"`;
+console.log(personalMessage);
+// QUESTION :3 OF 100 DAYS CHALLENGE
+let case1 = (personName.toLowerCase());
+console.log(case1);
+let case2 = (personName.toUpperCase());
+console.log(case2);
+let case3 = (personName.charAt(0).toLocaleUpperCase() + personName.toLocaleLowerCase().slice(1));
+console.log(case3);
+// Day one DONE!!!! 
---
+++ DAY 1/app1.ts
@@ -0,0 +1,26 @@
+// QUESTION :2 OF 100 DAYS CHALLENGE
+
+let personName : string = "Asharib"
+
+let personalMessage : string = `"Hello ${personName}, would you like to learn some TypeScript today?"`
+
+
+console.log(personalMessage);
+
+
+// QUESTION :3 OF 100 DAYS CHALLENGE
+
+let case1 :string = (personName.toLowerCase())
+
+console.log(case1);
+
+let case2 :string = (personName.toUpperCase())
+
+console.log (case2)
+
+
+let case3 :string = (personName.charAt(0).toLocaleUpperCase() + personName.toLocaleLowerCase().slice(1))
+
+console.log (case3)
+
+// Day one DONE!!!! 
