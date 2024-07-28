1769c62a571958219262e177360bb4d8320edd4f
muhammadSaifurrehman985 <159415417+muhammadSaifurrehman985@users.noreply.github.com>
2024-07-28 21:36:47 +0500

    Add files via upload

---
+++ DAY 2/app1.js
@@ -0,0 +1,13 @@
+"use strict";
+//QUESTION :1
+let quote = `"Albert Einstein once said, “A person who never made a mistake never tried anything new.”"`;
+console.log(quote);
+//QUESTION : 2
+let famous_person = "Albert Einstein";
+let message = "A person who never made a mistake never tried anything new";
+console.log(`"${famous_person} once said, “${message}.”"`);
+//QUESTION : 3 STRIPING NAMES
+let person_name = "\t\n  Albert Einstien  \t\n";
+console.log(person_name);
+console.log(person_name.trim());
+// DAY2 DONE !!!
---
+++ DAY 2/app1.ts
@@ -0,0 +1,22 @@
+//QUESTION :1
+
+let quote: string = `"Albert Einstein once said, “A person who never made a mistake never tried anything new.”"`;
+
+console.log(quote);
+
+//QUESTION : 2
+let famous_person: string = "Albert Einstein";
+let message: string =
+  "A person who never made a mistake never tried anything new";
+
+console.log(`"${famous_person} once said, “${message}.”"`);
+
+//QUESTION : 3 STRIPING NAMES
+
+let person_name: string = "\t\n  Albert Einstien  \t\n";
+
+console.log(person_name);
+
+console.log(person_name.trim());
+
+// DAY2 DONE !!!
